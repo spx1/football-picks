6bf11722440d2fe088aabf484f20fffe9f0475a1
Will Smith <smith.wg@gmail.com>
2021-11-15 21:55:55 -0500

    Scrape a single week of data

--- app/picks/service.py
+++ app/picks/service.py
@@ -1,12 +1,25 @@
 from typing import List
 from .model import CTeam
 from app import db
+from bs4 import BeautifulSoup
+
+
+URL="https://www.vegasinsider.com/nfl/matchups/matchups.cfm/week/{week}/season/{year}"
+
+def scrap(file: str):
+    with open(file,"rt") as f:
+        soup = BeautifulSoup( f.read() )
+        table = soup.find("table",class_='nfl-matchups')
+        for tr in table.tbody.find_all("tr"):
+            tds = tr.find_all('td')
+            if len(tds) > 2:
+                away = tds[0].find('div',style='font-weight: bold;').string
+                home = tds[2].find('div',style='font-weight: bold;').string
+                print(f'{away} @ {home}')
+
 
 def init_app():
-    db.session.add( CTeam(city="New York",nickname="Giants") )
-    db.session.add( CTeam(city="New York",nickname="Jets") )
-    db.session.add( CTeam(city="Kansas City", nickname="Chiefs") )
-    db.session.commit()
+    scrap('/home/will/documents/projects/football-picks/data/index.html?Year=2021&Week=10')
 
 class TeamService:
     @staticmethod
